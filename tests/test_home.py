442ac541328aacc5ccc6859efc4ea74d3e64db74
Chris McNutt <ctmcnutt1@gmail.com>
2024-02-29 15:46:12 -0500

    #1: Fixed spacing and added gherkin-style test case comments

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,9 +1,12 @@
 from playwright.sync_api import Page
 from pages.home import HomePage
 
-
+# Test Case: 
+# Given on home page
+# Then page title should be 'The Internet'
 
 def test_load_home_page(page: Page) -> None:
+
     # Initialize HomePage object
     homePage = HomePage(page)
 
